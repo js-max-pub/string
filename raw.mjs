ac2c47c12219bd0e5c52ed9347fcd5f26c9b5efc
Max Fechner <max.fechner@gmail.com>
2021-02-10 15:11:55 +0100

    - standalone (from lib)

---
+++ raw.mjs
@@ -0,0 +1,41 @@
+
+
+export function toTitleCase(string) {
+	return string.split(' ').filter(x => x)
+		.map(w => w[0].toUpperCase() + w.substr(1).toLowerCase())
+		.join(' ')
+}
+
+export function toCamelCase(string) {
+	// console.log('camel1',string,string.replace(/[^a-zäöüß1-9]/gi, ' '))
+	string = toTitleCase(string.replace(/[^a-zäöüß1-9]/gi, ' ')).replace(/\s/g, '')
+	// console.log('camel1',string)
+	// string[0] = string[0].toLowerCase();
+	return string[0].toLowerCase() + string.slice(1);
+}
+
+export function findGroups(string, regex) {
+	// console.log('find',string,regex)
+	return Array.from(string.matchAll(regex))
+		.map(i => i.slice(1)) // only groups, not full match
+	// .flat();
+}
+
+export function removeTags(string) {
+	return string.replace(/<.*?>/g, '')
+}
+
+export function blocks(string) {
+	return string.split('\r\n\r\n').flatMap(x => x.split('\r\r')).flatMap(x => x.split('\n\n'))
+}
+export function lines(string) {
+	return string.split('\r\n').flatMap(x => x.split('\r')).flatMap(x => x.split('\n'))
+}
+
+// https://stackoverflow.com/questions/8493195/how-can-i-parse-a-csv-string-with-javascript-which-contains-comma-in-data
+export function tsv(string) {
+	return string.split('\t')
+}
+export default {
+	toTitleCase, toCamelCase, findGroups, removeTags, blocks, lines, tsv
+}
